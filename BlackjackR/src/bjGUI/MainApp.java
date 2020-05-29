1520e840cba1bdd5e0575c0ddf8b1cb5287a55b0
craig <craig@LAPTOP-HS94FFLT.lan>
2020-05-29 14:38:27 +0100

    Add panels to GUI

--- BlackjackR/src/bjGUI/MainApp.java
+++ BlackjackR/src/bjGUI/MainApp.java
@@ -3,10 +3,13 @@ package bjGUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
 
 public class MainApp {
 
 	private JFrame frame;
+	private JPanel pnlIntroScreen;
 
 	/**
 	 * Launch the application.
@@ -36,8 +39,17 @@ public class MainApp {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 639, 616);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		pnlIntroScreen = new JPanel();
+		frame.getContentPane().add(pnlIntroScreen, "name_241053635896100");
+		pnlIntroScreen.setLayout(null);
+		
+		JPanel pnlGameScreen = new JPanel();
+		frame.getContentPane().add(pnlGameScreen, "name_241125091166600");
+		pnlGameScreen.setLayout(null);
 	}
 
 }
