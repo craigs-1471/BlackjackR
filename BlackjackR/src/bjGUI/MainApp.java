73b3b0e341f5728bda70d15f7e945c31245a47df
craig <craig@LAPTOP-HS94FFLT.lan>
2020-05-29 15:03:45 +0100

    Add functionality to play and exit buttons

--- BlackjackR/src/bjGUI/MainApp.java
+++ BlackjackR/src/bjGUI/MainApp.java
@@ -5,11 +5,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.CardLayout;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainApp {
 
 	private JFrame frame;
 	private JPanel pnlIntroScreen;
+	private JButton btnStartGame;
+	private JButton btnExitIntro;
+	private JPanel pnlGameScreen;
+	private JButton btnNewGame;
+	private JButton btnDeal;
+	private JButton btnTwist;
+	private JButton btnStick;
+	private JButton btnExit;
 
 	/**
 	 * Launch the application.
@@ -47,9 +58,53 @@ public class MainApp {
 		frame.getContentPane().add(pnlIntroScreen, "name_241053635896100");
 		pnlIntroScreen.setLayout(null);
 		
-		JPanel pnlGameScreen = new JPanel();
+		btnStartGame = new JButton("Start Game");
+		btnStartGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pnlGameScreen.setVisible(true);
+				pnlIntroScreen.setVisible(false);
+			}
+		});
+		btnStartGame.setBounds(90, 507, 89, 23);
+		pnlIntroScreen.add(btnStartGame);
+		
+		btnExitIntro = new JButton("Exit");
+		btnExitIntro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		btnExitIntro.setBounds(409, 507, 89, 23);
+		pnlIntroScreen.add(btnExitIntro);
+		
+		pnlGameScreen = new JPanel();
 		frame.getContentPane().add(pnlGameScreen, "name_241125091166600");
 		pnlGameScreen.setLayout(null);
+		
+		btnNewGame = new JButton("New Game");
+		btnNewGame.setBounds(34, 523, 89, 23);
+		pnlGameScreen.add(btnNewGame);
+		
+		btnDeal = new JButton("Deal");
+		btnDeal.setBounds(147, 523, 89, 23);
+		pnlGameScreen.add(btnDeal);
+		
+		btnTwist = new JButton("Twist");
+		btnTwist.setBounds(261, 523, 89, 23);
+		pnlGameScreen.add(btnTwist);
+		
+		btnStick = new JButton("Stick");
+		btnStick.setBounds(379, 523, 89, 23);
+		pnlGameScreen.add(btnStick);
+		
+		btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pnlIntroScreen.setVisible(true);
+				pnlGameScreen.setVisible(false);
+			}
+		});
+		btnExit.setBounds(497, 523, 89, 23);
+		pnlGameScreen.add(btnExit);
 	}
-
 }
